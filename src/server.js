0447d6a2311324174c395e2303126df2222f811c
adamfouche <adamfouche2002@gmail.com>
2022-05-28 09:27:32 +0700

    Config babeljs.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { mapOrder } from './utils/sort.js'
+import { mapOrder } from '~/utils/sorts'
 const app = express()
 
 const hostName = 'localhost'
 const port = 8057
 
 app.get('/', (req, res) => {
-  res.end('<h1>Hello World</h1><hr/>')
+  res.end('<h1>Tam giac everybody !!!</h1><hr/>')
 })
 
 app.listen(port, hostName, () => {
