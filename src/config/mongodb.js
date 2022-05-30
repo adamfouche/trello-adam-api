95fcf59a3a0e87c035613fef7cf6486183d33b2f
adamfouche <adamfouche2002@gmail.com>
2022-05-30 10:13:46 +0700

    Error

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,28 +1,22 @@
 import { MongoClient } from 'mongodb'
 import { env } from './environment'
 
+let dbInstance = null
+
 export const connectDB = async () => {
   const client = new MongoClient(env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  try {
-    // connect the client to the server
-    await client.connect()
-    console.log('Connect successfully to the server')
-
-    // List databases
-    await listDatabases(client)
-  } finally {
-    // Ensure the client will close when finish/error
-    await client.close()
-    console.log('Closed!')
-  }
+  // connect the client to the server
+  await client.connect()
+  // Assign clientDB to our dbInstance
+  dbInstance = client.db(env.DATABASE_NAME)
 }
-const listDatabases = async (client) => {
-  const databaseLists = await client.db().admin().listDatabases()
-  const databaseNames = databaseLists.databases
-  console.log('List of DB: ', databaseLists)
-  console.log('Name of the databases: ')
-  databaseNames.forEach((db) => console.log('- ', db.name))
+//   const databaseLists = await client.db().admin().listDatabases()
+
+// Get Database Instance
+export const getDB = () => {
+  if (!dbInstance) throw new Error('Unable to connect to MongoDB!')
+  return dbInstance
 }
